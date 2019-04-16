b2e595023e98444b3498ece157e782a669939a32
Talut TASGIRAN <iletisim@taluttasgiran.com.tr>
2019-04-16 15:51:22 +0300

    typescript definition issue solved

--- react-native-pin-view.d.ts
+++ react-native-pin-view.d.ts
@@ -4,22 +4,19 @@
 // TypeScript Version: 2.8
 declare module "react-native-pin-view" {
     import React from 'react';
-
     interface PinViewProps {
-        pinLength: number,
-        showInputs: boolean,
-        inputTextStyle: object,
-        buttonTextColor: string,
-        returnType: string,
-        buttonBgColor: string,
-        inputBgColor: string,
+        pinLength?: number,
+        showInputs?: boolean,
+        inputTextStyle?: object,
+        buttonTextColor?: string,
+        returnType?: string,
+        buttonBgColor?: string,
+        inputBgColor?: string,
         onComplete: () => void,
-        disabled: boolean,
-        inputActiveBgColor: string,
-        inputBgOpacity: number,
-        deleteText: string,
-        inputViewStyle?: object,
-        keyboardViewStyle?: object,
+        disabled?: boolean,
+        inputActiveBgColor?: string,
+        inputBgOpacity?: number,
+        deleteText?: string
     }
 
     const PinView: (props: PinViewProps) => React.Component<PinViewProps>;
